a9482037ed14ffab5e00ea4064eba1026148fe63
ansnsj3 <syl78918@gmail.com>
2026-03-06 17:31:08 +0300

    يي

--- OneSignalSDKWorker.js
+++
@@ -1,5 +0,0 @@
-// ================================================================
-// OneSignalSDKWorker.js
-// مطلوب من OneSignal في هذا المسار بالضبط
-// ================================================================
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
